eddb64d6ecdd63bd73c8d86e4fa59557ca9ab62a
Revisor01 <mail@simonluthe.de>
2025-07-23 16:31:30 +0200

    fix

--- backend/push/apns.js
+++ backend/push/apns.js
@@ -2,7 +2,7 @@ const apn = require('apn');
 
 const options = {
   token: {
-    key: './AuthKey_A29U7SN796.p8', // dein .p8 Key Pfad
+    key: './push/AuthKey_A29U7SN796.p8', // dein .p8 Key Pfad
     keyId: 'A29U7SN796',
     teamId: 'J459G9CJT5',
   },
