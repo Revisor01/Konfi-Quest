fa8912a00932929eb86a451cb83dce545504f2c8
Revisor01 <mail@simonluthe.de>
2025-08-01 12:22:45 +0200

    fix smtp

--- backend/server.js
+++ backend/server.js
@@ -29,8 +29,8 @@ const db = require('./database');
 
 const SMTP_CONFIG = {
   host: process.env.SMTP_HOST || 'server.godsapp.de',
-  port: process.env.SMTP_PORT || 465,
-  secure: true, // true for 465, false for other ports
+  port: process.env.SMTP_PORT || 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER || 'team@konfi-quest.de',
     pass: process.env.SMTP_PASS || 'NkqFQuTx$877Si!6Pp'
